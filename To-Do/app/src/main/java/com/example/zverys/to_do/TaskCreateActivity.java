f36a96d3903c0e34cf4d74c62042e869932e05f4
DYNEKS\Linas <rlinasr@gmail.com>
2018-03-01 17:13:04 +0200

    Daugiau užduoties kūrimo lango

--- To-Do/app/src/main/java/com/example/zverys/to_do/TaskCreateActivity.java
+++ To-Do/app/src/main/java/com/example/zverys/to_do/TaskCreateActivity.java
@@ -2,6 +2,8 @@ package com.example.zverys.to_do;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TaskCreateActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class TaskCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_create);
+        //get the spinner from the xml.
+        Spinner dropdown = (Spinner) findViewById(R.id.kategorijos);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"choose category", "Summer", "place holder"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }
