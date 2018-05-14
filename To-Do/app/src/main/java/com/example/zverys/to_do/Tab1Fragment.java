c657827c47a3a91b986a01ea02c2894ffc46cf6e
Augmas <augispk@gmail.com>
2018-05-14 14:24:01 +0300

    no message

--- To-Do/app/src/main/java/com/example/zverys/to_do/Tab1Fragment.java
+++ To-Do/app/src/main/java/com/example/zverys/to_do/Tab1Fragment.java
@@ -2,11 +2,15 @@ package com.example.zverys.to_do;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 
 
 public class Tab1Fragment extends Fragment {
@@ -29,5 +33,6 @@ public class Tab1Fragment extends Fragment {
 
         return view;
     }
+
     }
 
